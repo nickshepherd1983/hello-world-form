7dd8295e5c32956c1cad3a8a6ceeed1c4ff212db
Nick Shepherd <nshepherd@uxceclipse.com>
2015-07-08 15:57:23 -0700

    Made the button bigger.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 61);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(260, 237);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "&Close the app!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
